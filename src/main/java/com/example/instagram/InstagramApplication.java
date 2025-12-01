a8cae84f624db22ef9810460afb7549ba3926125
sshkim1013 <sshkim1013@naver.com>
2025-12-01 14:56:07 +0900

    1201 JPA Auditing 활성화

--- src/main/java/com/example/instagram/InstagramApplication.java
+++ src/main/java/com/example/instagram/InstagramApplication.java
@@ -2,8 +2,10 @@ package com.example.instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // BaseEntity에서 설정한 @EntityListeners(AuditingEntityListener.class) 사용을 위해서 설정
 public class InstagramApplication {
 
     public static void main(String[] args) {
