95a7bab3e4fd96a499430eb19b071abc73378096
sshkim1013 <sshkim1013@naver.com>
2025-12-04 11:22:21 +0900

    1204 EntityGraph 적용

--- src/main/java/com/example/instagram/repository/CommentRepository.java
+++ src/main/java/com/example/instagram/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package com.example.instagram.repository;
 
 import com.example.instagram.entity.Comment;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @EntityGraph(attributePaths = {"user"})
     List<Comment> findByPostIdOrderByCreatedAtDesc(Long postId);
 
     long countByPostId(Long id);
--- src/main/java/com/example/instagram/repository/PostRepository.java
+++ src/main/java/com/example/instagram/repository/PostRepository.java
@@ -2,14 +2,21 @@ package com.example.instagram.repository;
 
 import com.example.instagram.entity.Post;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @EntityGraph(attributePaths = {"user"})
     List<Post> findAllByOrderByCreatedAtDesc();
 
+    @EntityGraph(attributePaths = {"user"})
     List<Post> findByUserIdOrderByCreatedAtDesc(Long userId);
 
     long countByUserId(Long userId);
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Post> findById(Long id);
 
 }
