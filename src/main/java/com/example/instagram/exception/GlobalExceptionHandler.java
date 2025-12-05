5db89dfb02b550b863b711095636b6f5b4a806c2
sshkim1013 <sshkim1013@naver.com>
2025-12-05 13:06:06 +0900

    1205 500 페이지 추가

--- src/main/java/com/example/instagram/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/instagram/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.instagram.exception;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -11,6 +12,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoResourceFoundException.class)
     public String handleNoResourceFound(NoResourceFoundException ex) {
         return "error/404";
+    }
+
+    // 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error/500";
     }
 
 }
