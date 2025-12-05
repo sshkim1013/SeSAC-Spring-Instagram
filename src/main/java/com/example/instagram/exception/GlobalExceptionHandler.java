5d03445210eff4c45c37eb8a95219bd426a2b883
sshkim1013 <sshkim1013@naver.com>
2025-12-05 11:38:07 +0900

    1205 404 페이지 추가

---
+++ src/main/java/com/example/instagram/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.instagram.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 404
+    @ExceptionHandler(NoResourceFoundException.class)
+    public String handleNoResourceFound(NoResourceFoundException ex) {
+        return "error/404";
+    }
+
+}
